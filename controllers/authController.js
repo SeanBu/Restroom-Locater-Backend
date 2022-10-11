c915556057fbd3a42f5e77441ead56537d79a9d0
SeanBu <seanbuchas@gmail.com>
2022-10-11 14:21:51 -0400

    create register route in authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,24 @@
+require("dotenv").config();
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../models/')
 
+
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
+    const user = req.body;
 
+    const submittedUserName = await User.findOne({ username: user.username });
+    const submittedEmail = await User.findOne({ email: user.email });
+
+    if (submittedEmail || submittedUserName) {
+        res.json({ message: "Username or Email already in use." })
+    } else {
+        user.password = await bcrypt.hash(req.body.password, process.env.ROUNDS)
+    }
 });
 
 router.post('/login', async (req, res) => {
